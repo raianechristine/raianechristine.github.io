0880a0acffc399d99f293a11ce422b3e3f68ba54
raiane <raianechristine@ioasys.com.br>
2020-07-21 18:09:13 -0300

    Updates

---
+++ precache-manifest.071edf068cf23e04873abb9ada24aa60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472ed2e7791456d112ddcdc2a8b690da",
+    "url": "/raiane-christine/index.html"
+  },
+  {
+    "revision": "4e23dc5abe4dcf2844fc",
+    "url": "/raiane-christine/static/js/2.dff47323.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/raiane-christine/static/js/2.dff47323.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c66f8c5c16d612d8a514",
+    "url": "/raiane-christine/static/js/main.13cf4300.chunk.js"
+  },
+  {
+    "revision": "cac5b83b1e7cc387e765",
+    "url": "/raiane-christine/static/js/runtime-main.241a6f1b.js"
+  },
+  {
+    "revision": "a2f4251ce5350bb80f3f1702713ae771",
+    "url": "/raiane-christine/static/media/Gilroy-Bold.a2f4251c.woff"
+  },
+  {
+    "revision": "59779aac314ee0dcce7dcd4c88c77161",
+    "url": "/raiane-christine/static/media/coffee.59779aac.svg"
+  }
+]);
